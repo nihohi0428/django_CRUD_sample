1f9d7138b12b959a6b40635eb6bb7c4cc4552d4a
nihohi <nihohi@hotmail.com>
2013-11-22 09:15:49 +0000

    implemented Read API

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-
 from django.shortcuts import render_to_response, get_object_or_404
 from django.shortcuts import render
 from django.template import RequestContext
 from django.utils.encoding import smart_unicode
+from django.utils import simplejson
+
 import sys
 import time
 import pytz
@@ -28,26 +29,46 @@ def __judgeGrade(score):
         grade = "gradeU"
     return grade
         
-def read(request, sort_key='-score'):
-    if request.GET.has_key('sort_key'):
-        sort_key = request.GET['sort_key'] #@todo:[SMT] refactor, GET? POST? both??
-        
-    records = models.Record.get_all().order_by(sort_key)
-    records_tmp = [ {'id'          : str(record.id),
-                     'username'    : smart_unicode(record.username),
-                     'description' : smart_unicode(record.description),
-                     'score'       : str(record.score),
-                     'datetime'    : str(record.datetime.astimezone(pytz.timezone('Asia/Tokyo'))),
-                     'grade'       : __judgeGrade(record.score),
-                     } for record in records ]
+def index(request):
+    records_dict_list =  __read(request)
     site.addsitedir('site-packages','.')
     path = sys.path
     # for csrf
     return render_to_response('index.html',
-                              { 'records': records_tmp,
+                              { 'records': records_dict_list,
                                 'path'   : path
                                 },
                               context_instance=RequestContext(request))
+
+
+def __createHttpResponseJson( results ) :
+    response = HttpResponse(
+                            mimetype     = 'application/json; charset=utf-8',
+                            content_type = 'application/json; charset=utf-8',
+                            )
+    response.__setitem__( 'Access-Control-Allow-Origin', '*' )
+    response.content = simplejson.dumps( results, ensure_ascii = False )
+    
+    return response
+
+def __read(request, sort_key='-score'):
+    if request.GET.has_key('sort_key'):
+        sort_key = request.GET['sort_key'] #@todo:[SMT] refactor, GET? POST? both??
+        
+    records = models.Record.get_all().order_by(sort_key)
+    records_dict_list = [ {'id'          : str(record.id),
+                           'username'    : smart_unicode(record.username),
+                           'description' : smart_unicode(record.description),
+                           'score'       : str(record.score),
+                           'datetime'    : str(record.datetime.astimezone(pytz.timezone('Asia/Tokyo'))),
+                           'grade'       : __judgeGrade(record.score),
+                           } for record in records ]
+    return records_dict_list
+
+def read(request, sort_key='-score'):
+    records_dict_list = __read(request, sort_key)
+    records_json = __createHttpResponseJson( records_dict_list )
+    return records_json
 
 def create(request):
     record = models.Record( username = request.POST['username'],
@@ -55,7 +76,7 @@ def create(request):
                             score = request.POST['score'],
                             )
     record.save()
-    return read(request)
+    return index(request)
 
 def update(request):
     record = models.Record.objects.get(id=request.POST['id'])
@@ -64,10 +85,10 @@ def update(request):
     record.score = request.POST['score']
 
     record.save()
-    return read(request)
+    return index(request)
 
 def delete(request):
     record = models.Record.objects.get(id=request.POST['id'])
     record.delete()
-    return read(request)
+    return index(request)
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,20 +6,21 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
                        # Examples:
-                           # url(r'^$', 'mysite.views.home', name='home'),
+                       # url(r'^$', 'mysite.views.home', name='home'),
                        # url(r'^mysite/', include('mysite.foo.urls')),
                        
                        # Uncomment the admin/doc line below to enable admin documentation:
-                           # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        
                        # Uncomment the next line to enable the admin:
-                           # url(r'^admin/', include(admin.site.urls)),
-
+                       # url(r'^admin/', include(admin.site.urls)),
+                       
                        #(r'^top/$', 'myapp.views.main')
                        url(r'^$', 'myapp.views.main'),
-                       url(r'^mysite/myapp/$',        'myapp.views.read'  ),
+                       url(r'^mysite/myapp/$',        'myapp.views.index' ),
                        url(r'^mysite/myapp/create/$', 'myapp.views.create'),
+                       url(r'^mysite/myapp/read/$',   'myapp.views.read'  ),    
                        url(r'^mysite/myapp/update/$', 'myapp.views.update'),
                        url(r'^mysite/myapp/delete/$', 'myapp.views.delete'),
-)
+                       )
 
